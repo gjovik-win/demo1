90b8955a4d3dfe32baaae989235cdddf7a6df135
Hans Kristian Nordengen <hans.kristian.nordengen@gmail.com>
2018-11-21 17:33:59 +0100

    Added the usage of environment variables

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
-let   app = express()
+const app = express()
 
-let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
+const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const hostname    = process.env.HOSTNAME || "Generic hostname"
+const application = process.env.APPLICATION || "Generic application"
+
+app.get('/', (req, res) => res.send(`My name is ${application} and I am running in the pod ${hostname}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const healthApp = express()
+const healthPort = port + 1
+healthApp.get('/ready', (req, res)  => res.send(`Jepp. Jeg er klar!!`))
+healthApp.get('/alive', (req, res)  => res.send(`Er da fremdeles i live!`))
+healthApp.listen(healthPort, () => console.log(`Example app listening on port ${healthPort} for health reqests!`))
