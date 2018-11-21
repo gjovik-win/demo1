7771bcd556601289dff79ab5430ff6132655ff21
Hans Kristian Nordengen <hans.kristian.nordengen@gmail.com>
2018-11-22 00:02:55 +0100

    Added more functinality to read and alive endpoints

--- server.js
+++ server.js
@@ -10,32 +10,33 @@ const application = process.env.APPLICATION || "Generic application"
 let httpCode = 200
 let readyCode = 200
 
-app.get('/', (req, res) => {
+function getTs() {
   const date = new Date()
-  res.status(httpCode).send(`${date.toISOString()}: ${application} in pod ${hostname}` + (httpCode == 200?"":" is really sick"))
+  return(date.toISOString())
+}
+
+app.get('/', (req, res) => {
+  res.status(httpCode).send(`${getTs()}: ${application} in pod ${hostname}` + (httpCode == 200?"":" is really sick"))
 })
 
 app.put('/kill', (req, res) => {
   httpCode = 503
-  const date = new Date()
-  res.status(httpCode).send(`${date.toISOString()}: ${application} in pod ${hostname} - you killed me!!`)
+  res.status(httpCode).send(`${getTs()}: ${application} in pod ${hostname} - you killed me!!`)
 })
 
-app.put('/getready', (req, res) => {
-  readyCode = 200
-  const date = new Date()
-  res.status(httpCode).send(`${date.toISOString()}: ${application} in pod ${hostname} - I'm ready!!`)
-})
-
-app.put('/backoff', (req, res) => {
-  readyCode = 503
-  const date = new Date()
-  res.status(httpCode).send(`${date.toISOString()}: ${application} in pod ${hostname} - I'm not talking to you!!`)
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Demo app listening on port ${port}!`))
 
 const healthApp = express()
-healthApp.get('/ready', (req, res) => { res.status(readyCode).send(httpCode==200?`Jepp. Jeg er klar!!`:"Glem det!") })
+healthApp.put('/getready', (req, res) => {
+  readyCode = 200
+  res.status(httpCode).send(`${getTs()}: ${application} in pod ${hostname} - I'm ready!!`)
+})
+
+healthApp.put('/backoff', (req, res) => {
+  readyCode = 503
+  res.status(httpCode).send(`${getTs()}: ${application} in pod ${hostname} - I'm not talking to you!!`)
+})
+
+healthApp.get('/ready', (req, res) => { res.status(readyCode).send(readyCode==200?`Jepp. Jeg er klar!!`:"Glem det!") })
 healthApp.get('/alive', (req, res) => { res.status(httpCode).send(httpCode==200?`Er da fremdeles i live!`:"Hjelp, jeg dør!") })
-healthApp.listen(healthPort, () => console.log(`Example app listening on port ${healthPort} for health reqests!`))
+healthApp.listen(healthPort, () => console.log(`Demo app listening on port ${healthPort} for health reqests!`))
