ff38f57c93cbbf3ca8afc3a7c7135414107a6324
Hans Kristian Nordengen <hans.kristian.nordengen@gmail.com>
2018-11-21 19:32:03 +0100

    Added kill demo

--- server.js
+++ server.js
@@ -3,19 +3,27 @@ const app = express()
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+const healthPort = port + 1
 
 const hostname = process.env.HOSTNAME || "Generic hostname"
 const application = process.env.APPLICATION || "Generic application"
 
+let httpCode = 200
+
 app.get('/', (req, res) => {
   const date = new Date()
-  res.send(`${date.toISOString()}: ${application} in pod ${hostname}`)
+  res.status(httpCode).send(`${date.toISOString()}: ${application} in pod ${hostname}` + (httpCode == 200?"":" is really sick"))
+})
+
+app.get('/kill', (req, res) => {
+  httpCode = 503
+  const date = new Date()
+  res.status(httpCode).send(`${date.toISOString()}: ${application} in pod ${hostname} - you killed me!!`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 const healthApp = express()
-const healthPort = port + 1
-healthApp.get('/ready', (req, res) => res.send(`Jepp. Jeg er klar!!`))
-healthApp.get('/alive', (req, res) => res.send(`Er da fremdeles i live!`))
+healthApp.get('/ready', (req, res) => { res.status(httpCode).send(httpCode==200?`Jepp. Jeg er klar!!`:"Glem det!") })
+healthApp.get('/alive', (req, res) => { res.status(httpCode).send(httpCode==200?`Er da fremdeles i live!`:"Hjelp, jeg dør!") })
 healthApp.listen(healthPort, () => console.log(`Example app listening on port ${healthPort} for health reqests!`))
